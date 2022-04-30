02fd0ffa8ca1c6621c664256a09c11bcfdedf14b
Joaco <rosariodota3@gmail.com>
2022-04-30 18:55:04 -0300

    new internal login screen ui

--- app/src/main/java/com/puntogris/posture/ui/login/InternalLoginFragment.kt
+++ app/src/main/java/com/puntogris/posture/ui/login/InternalLoginFragment.kt
@@ -1,10 +1,8 @@
 package com.puntogris.posture.ui.login
 
-import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.puntogris.posture.R
 import com.puntogris.posture.databinding.FragmentInternalLoginBinding
 import com.puntogris.posture.ui.base.BaseLoginFragment
@@ -16,17 +14,12 @@ class InternalLoginFragment :
     BaseLoginFragment<FragmentInternalLoginBinding>(R.layout.fragment_internal_login) {
 
     override val binding by viewBinding(FragmentInternalLoginBinding::bind)
+
     override val viewModel: LoginViewModel by viewModels()
+
     override val progressBar: ProgressBar
         get() = binding.progressBar
+
     override val loginWithGoogleButton: View
         get() = binding.loginWithGoogleButton
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.continueAnonymouslyButton.setOnClickListener {
-            findNavController().navigateUp()
-        }
-    }
 }
