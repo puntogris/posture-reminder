f600cc1945313563617bb28f1e2a4b6e6a399da4
puntogris <rosariodota3@gmail.com>
2023-02-25 17:30:20 -0300

    changed exercise colors

--- app/src/main/java/com/puntogris/posture/data/datasource/local/LocalDataSource.kt
+++ app/src/main/java/com/puntogris/posture/data/datasource/local/LocalDataSource.kt
@@ -11,14 +11,14 @@ object LocalDataSource {
             R.string.brugger_exercise_title,
             R.string.brugger_exercise_summary,
             60,
-            R.color.card1,
+            R.color.red_400,
             R.drawable.ic_brugger_exercise,
             R.array.brugger_exercise_steps
         ),
         Exercise(
             R.string.back_extensions_exercise_title,
             R.string.back_extension_exercise_summary,
-            50, R.color.card2,
+            50, R.color.green_400,
             R.drawable.ic_back_extension_exercise,
             R.array.back_extension_exercise_steps
         ),
@@ -26,7 +26,7 @@ object LocalDataSource {
             R.string.chin_tuck_exercise_title,
             R.string.chin_tuck_exercise_summary,
             50,
-            R.color.card3,
+            R.color.purple_400,
             R.drawable.ic_chin_tuck_exercise,
             R.array.chin_tuck_exercise_steps
         ),
@@ -34,7 +34,7 @@ object LocalDataSource {
             R.string.side_bend_exercise_title,
             R.string.side_bend_exercise_summary,
             60,
-            R.color.card4,
+            R.color.yellow_400,
             R.drawable.ic_side_bend_exercise,
             R.array.side_bend_exercise_steps
         )
